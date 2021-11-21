56472b0ec15f7300c9d4ee1a8ba1301a03b92c5e
Himaja Penumaka <himaja.mgl2@gmail.com>
2021-11-21 15:48:09 +0530

    Create Movie.java

---
+++ Movie.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+
+public class Movie {
+
+    private String title, studio, rating;
+    public Movie(String title, String studio, String rating){
+        this.title= title;
+        this.studio= studio;
+        this.rating= rating;
+    }
+
+    public Movie(String title, String studio){
+        this.title= title;
+        this.studio= studio;
+        this.rating= "PG";
+    }
+
+    public String getRating() {
+        return this.rating;
+    }
+
+    public String getStudio(){
+        return this.studio;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public Movie[] getPg(Movie[] movies){
+        Movie result[]= new Movie[movies.length];
+        int k=0;
+        for(Movie m: movies){
+            if(m.getRating().equals("PG"))
+                result[k++] = m;
+        }
+        return Arrays.copyOfRange(result, 0, k);
+    }
+
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // creating and initializing three Movie objects
+        Movie m1 = new Movie("Indiana Jones", "Raiders", "PG");
+        Movie m2 = new Movie("Deadpool", "Fox", "R");
+        Movie m3 = new Movie("Life Of Pi", "20th Century", "PG");
+
+        // creating and initializing a Movie array
+        Movie[] movies = {m1, m2, m3};
+
+        // calling the getPg() method
+        Movie[] pgRatedMovies = m1.getPg(movies);
+        for(Movie m: pgRatedMovies){
+            System.out.println(m.getTitle());
+        }
+
+    }
+}
